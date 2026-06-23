15d323e6ecdde789b78cf26dd6fb770e5f8c1f29
Soumya Gupta <166936873+soumya27062005@users.noreply.github.com>
2026-06-23 23:12:23 +0530

    Change import statement for Projects component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/sections/Hero";
 import { About } from "@/components/sections/About";
-import { Projects } from "@/components/sections/Projects";
+import Projects from "@/components/sections/Projects";
 import { Certificates } from "@/components/sections/Certificates";
 import { Experience } from "@/components/sections/Experience";
 import { Contact } from "@/components/sections/Contact";
